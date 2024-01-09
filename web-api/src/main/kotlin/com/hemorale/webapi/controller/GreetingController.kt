0bf11893a680522a47154e4bbc17d0aa2eb874b8
Hector Rocha Morales <hector.rocha@carvajal.com>
2024-01-08 19:19:37 -0600

    [API Kotlin] UNIR Docker usando lenguaje kotlin

--- web-api/src/main/kotlin/com/hemorale/webapi/controller/GreetingController.kt
+++ web-api/src/main/kotlin/com/hemorale/webapi/controller/GreetingController.kt
@@ -3,10 +3,10 @@ package com.hemorale.webapi.controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RestController("greeting")
+@RestController
 class GreetingController {
 
-	@GetMapping
+	@GetMapping("/greeting")
 	fun get(): Array<String> {
 		return arrayOf(
 			"Hello World kotlin web API!",
